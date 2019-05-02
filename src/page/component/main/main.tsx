3d0eb29e7557e45e19419f18299cc60d1a8a7fcf
akat32 <baduk32127@gmail.com>
2019-05-02 18:56:02 +0900

    add main page & scroll

--- src/page/component/main/main.tsx
+++ src/page/component/main/main.tsx
@@ -1,8 +1,25 @@
 import React, { Component, useState, useEffect} from 'react'
 
+import { Link } from 'react-router-dom'
+
+import './main.scss'
 export const Main = () => {
     return (
-        <div>
+        <div className = "mainPage">
+            <div className = "remoteController">
+                <a href = '#page1'><div className = "circle c1"></div></a>
+                <a href = '#page2'><div className = "circle c2"></div></a>
+                <a href = '#page3'><div className = "circle c3"></div></a>
+                <a href = '#page4'><div className = "circle c4"></div></a>
+                <a href = '#page5'><div className = "circle c5"></div></a>
+            </div>
+            <div id = "page1">
+                <div className = "blockImg"></div>            
+            </div>
+            <div id = "page2">p2</div>
+            <div id = "page3">p3</div>
+            <div id = "page4">p4</div>
+            <div id = "page5">p5</div>
         </div>
     )
 }
