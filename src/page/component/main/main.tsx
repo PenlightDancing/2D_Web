d6d473640f84ea63187c846f7e466d7c29d04be1
akat32 <baduk32127@gmail.com>
2019-05-02 19:29:06 +0900

    add scroll chk

--- src/page/component/main/main.tsx
+++ src/page/component/main/main.tsx
@@ -1,10 +1,30 @@
 import React, { Component, useState, useEffect} from 'react'
 
+import $ from "jquery";
+
+
 import { Link } from 'react-router-dom'
 
 import './main.scss'
 export const Main = () => {
-    return (
+    const [page = 1, setPage] = useState()
+    useEffect (()=>{
+        var position = $(window). scrollTop();
+        
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+            if (scroll > position) {
+                console.log('scrollDown');
+                $('div').text('Scrolling Down Scripts');
+            }
+            else {
+                console.log('scrollUp');
+                $('div').text('Scrolling Up Scripts');
+            }
+            position = scroll;
+        });
+    })
+    return (  
         <div className = "mainPage">
             <div className = "remoteController">
                 <a href = '#page1'><div className = "circle c1"></div></a>
