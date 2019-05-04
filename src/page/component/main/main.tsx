9fab1c5282208f9bf4b3877858c235e50519c8bd
akat32 <baduk32127@gmail.com>
2019-05-05 00:41:53 +0900

    add first apge

--- src/page/component/main/main.tsx
+++ src/page/component/main/main.tsx
@@ -9,32 +9,13 @@ import './main.scss'
 export const Main = () => {
     const [page = 1, setPage] = useState()
     useEffect (()=>{
-        var position = $(window). scrollTop();
-        
-        $(window).scroll(function() {
-            var scroll = $(window).scrollTop();
-            if (scroll > position) {
-                console.log('scrollDown');
-                $('div').text('Scrolling Down Scripts');
-            }
-            else {
-                console.log('scrollUp');
-                $('div').text('Scrolling Up Scripts');
-            }
-            position = scroll;
-        });
+
     })
     return (  
         <div className = "mainPage">
-            <div className = "remoteController">
-                <a href = '#page1'><div className = "circle c1"></div></a>
-                <a href = '#page2'><div className = "circle c2"></div></a>
-                <a href = '#page3'><div className = "circle c3"></div></a>
-                <a href = '#page4'><div className = "circle c4"></div></a>
-                <a href = '#page5'><div className = "circle c5"></div></a>
-            </div>
             <div id = "page1">
-                <div className = "blockImg"></div>            
+                <div className = "blockImg"></div>
+                <div className = "logo"></div>
             </div>
             <div id = "page2">p2</div>
             <div id = "page3">p3</div>
