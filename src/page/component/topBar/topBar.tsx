6bfaf70adb7dad6c1869584e10d375f9f97f2e38
akat32 <baduk32127@gmail.com>
2019-05-09 08:48:24 +0900

    add languge memu

--- src/page/component/topBar/topBar.tsx
+++ src/page/component/topBar/topBar.tsx
@@ -5,21 +5,25 @@ import './topBar.scss'
 export const TopBar = () => {
     return (
        <div className = "topBar">
-           <a href = '#divpage1'><div className = "logo"></div></a>
-           <ul>
-               <a href = "#page2"><li>게임</li></a>
-               <a href = "#page3"><li>팀 소개</li></a>
-               <a href = "#page4"><li>뉴스</li></a>
-               <a href = "#page5"><li>컨택</li></a>
-               <a href = "#page6"><li>약관</li></a>
-               <li>
+            <a href = '#divpage1'><div className = "logo"></div></a>
+            <ul>
+                <a href = "#page2"><li>게임</li></a>
+                <a href = "#page3"><li>팀 소개</li></a>
+                <a href = "#page4"><li>뉴스</li></a>
+                <a href = "#page5"><li>컨택</li></a>
+                <a href = "#page6"><li>약관</li></a>
+                <li>
                     <div className = "langugeIcon">
                         <div className = "icon"></div>
                         <p className = "text">KR</p>
                         <div className = "downIcon"></div>
                     </div>
                 </li>
-           </ul>
+            </ul>
+            <div className = "langugeMenu">
+                <div className = "Eng">English</div>
+                <div className = "Kor">한국어</div>
+            </div>
        </div>
     )
 }
