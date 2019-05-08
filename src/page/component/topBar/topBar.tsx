22bcc785edfd1e3e710e430d8a56bee1c7541c9f
akat32 <baduk32127@gmail.com>
2019-05-09 08:54:16 +0900

    add function in langugeMenu

--- src/page/component/topBar/topBar.tsx
+++ src/page/component/topBar/topBar.tsx
@@ -2,7 +2,17 @@ import React, { useEffect, useState } from 'react'
 
 import './topBar.scss'
 
+import $ from 'jquery'
+
 export const TopBar = () => {
+    const [menu = 'none', setMenu] = useState()
+    useEffect ( ()=> {
+        $('html').click((e)=>{
+            if(!$(e.target).hasClass('langugeMenu') && !$(e.target).hasClass('Eng') && !$(e.target).hasClass('Kor')) {
+                setMenu('none')
+            }
+        })
+    })
     return (
        <div className = "topBar">
             <a href = '#divpage1'><div className = "logo"></div></a>
@@ -12,7 +22,10 @@ export const TopBar = () => {
                 <a href = "#page4"><li>뉴스</li></a>
                 <a href = "#page5"><li>컨택</li></a>
                 <a href = "#page6"><li>약관</li></a>
-                <li>
+                <li onClick = {()=>{
+                    if(menu === 'block') setMenu('none');
+                    else setMenu('block');
+                }}>
                     <div className = "langugeIcon">
                         <div className = "icon"></div>
                         <p className = "text">KR</p>
@@ -20,7 +33,7 @@ export const TopBar = () => {
                     </div>
                 </li>
             </ul>
-            <div className = "langugeMenu">
+            <div className = "langugeMenu" style = {{display: menu}}>
                 <div className = "Eng">English</div>
                 <div className = "Kor">한국어</div>
             </div>
