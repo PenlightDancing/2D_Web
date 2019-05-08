b1f30d0654bb7e86907c9111af35a5e207e8c655
akat32 <baduk32127@gmail.com>
2019-05-09 05:42:43 +0900

    modified page1 link

--- src/page/component/topBar/topBar.tsx
+++ src/page/component/topBar/topBar.tsx
@@ -5,7 +5,7 @@ import './topBar.scss'
 export const TopBar = () => {
     return (
        <div className = "topBar">
-           <a href = '#page1'><div className = "logo"></div></a>
+           <a href = '#divpage1'><div className = "logo"></div></a>
            <ul>
                <a href = "#page2"><li>게임</li></a>
                <a href = "#page3"><li>팀 소개</li></a>
