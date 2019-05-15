598088c7448c9bb403996b3f65b9973fd75687d2
akat32 <baduk32127@gmail.com>
2019-05-15 22:12:10 +0900

    fix language btn

--- src/page/component/main/main.tsx
+++ src/page/component/main/main.tsx
@@ -8,9 +8,6 @@ import { Link } from 'react-router-dom'
 import './main.scss'
 export const Main = () => {
     const [page = 1, setPage] = useState()
-    useEffect (()=>{
-
-    })
     return (
         <div className = "mainPage">
             <TopBar/>
--- src/page/component/topBar/topBar.tsx
+++ src/page/component/topBar/topBar.tsx
@@ -5,14 +5,18 @@ import './topBar.scss'
 import $ from 'jquery'
 
 export const TopBar = () => {
-    const [menu = 'none', setMenu] = useState()
+    const [menu, setMenu] = useState(false)
     useEffect ( ()=> {
-        $('html').click((e)=>{
-            if(!$(e.target).hasClass('langugeMenu') && !$(e.target).hasClass('Eng') && !$(e.target).hasClass('Kor')) {
-                setMenu('none')
+        $('html').click( (e)=>{
+            if(!$(e.target).hasClass('downIcon') && !$(e.target).hasClass('icon') &&!$(e.target).hasClass('text') &&!$(e.target).hasClass('btnbtn') && !$(e.target).hasClass('langugeMenu') && !$(e.target).hasClass('Eng') && !$(e.target).hasClass('Kor')) {
+                setMenu(false)
             }
         })  
     })
+    function globalMenu () {
+        setMenu(!menu)
+    }
+    const menuDisplay =  menu ? {} : {display : 'none'}
     return (
        <div className = "topBar">
             <a href = '#divpage1'><div className = "logo"></div></a>
@@ -22,10 +26,7 @@ export const TopBar = () => {
                 <a href = "#page4"><li>뉴스</li></a>
                 <a href = "#page5"><li>컨택</li></a>
                 <a href = "#page6"><li>약관</li></a>
-                <li onClick = {()=>{
-                    if(menu === 'block') setMenu('none');
-                    else setMenu('block');
-                }}>
+                <li className = "btnbtn" onClick = { globalMenu }>
                     <div className = "langugeIcon">
                         <div className = "icon"></div>
                         <p className = "text">KR</p>
@@ -33,7 +34,7 @@ export const TopBar = () => {
                     </div>
                 </li>
             </ul>
-            <div className = "langugeMenu" style = {{display: menu}}>
+            <div className = "langugeMenu" style = {menuDisplay}>
                 <div className = "Eng">English</div>
                 <div className = "Kor">한국어</div>
             </div>
