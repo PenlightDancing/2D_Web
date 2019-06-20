d639a75c3369c72b78cef88a5fbbcd8ab2efc71f
= <=>
2019-06-20 22:48:28 +0900

    set new Function in App

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import { Main, Mobile } from '../page'
 
 const App: React.FC = () => {
+  const [isMobileDevice = false, setBool] = useState()
+  function isMobile() {
+    var UserAgent = navigator.userAgent;
+    if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null)
+    {
+        return true;
+    }else{
+        return false;
+    }
+  }    
+  useEffect (()=>{
+      if (isMobile())
+      {
+        setBool( true )
+      }
+      else
+        setBool ( false )
+  })
   return (
     <div className="App">
       <Router>
         <div>
-          <Route exact path = '/' component = { Main }/>
-          <Route exact path = '/mobile' component = { Mobile }/>
+          { !isMobileDevice ? (
+            <Route exact path = '/' component = { Main }/>
+          ) : (
+            <Route exact path = '/' component = { Mobile }/>
+          )}
         </div>
       </Router>
     </div>
